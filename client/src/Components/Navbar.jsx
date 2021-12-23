e8aace6e079c254874490230cce3e7aa43b700dd
Oxamee <87671892+Oxamee@users.noreply.github.com>
2021-12-23 20:58:54 +0100

    navBar first version done with commenting

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import "./Navbar.css";
+import { Col, Row } from "reactstrap";
 function Navbar() {
   return (
-    <div>
-      <h1>hey</h1>
+    <div className="allTheNavBar" style={{ backgroundColor: "black" }}>
+      <Row>
+        <Col>
+          <div className="brandNameContainer">
+            <h1 className="brandNameNavBar">dolla</h1>
+          </div>
+        </Col>
+        <Col>
+          <div className="widgetContainer">
+            <h2 className="widgetName">About</h2>
+            <h2 className="widgetName">Discover</h2>
+            <h2 className="widgetName">Services</h2>
+            <h2 className="widgetName">Sign Up</h2>
+          </div>
+        </Col>
+        <Col>
+          <div className="signInContainer">
+            <button className="signInButtonNavBar">Sign In</button>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
